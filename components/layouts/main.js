b9f9e73825b7c0705b7e6e7fa6b8a8d61134c6d0
ClintonWeeYuan <clinton_wee@hotmail.com>
2022-03-27 18:34:34 +0800

    first commit

---
+++ components/layouts/main.js
@@ -0,0 +1,24 @@
+import {Box, Container} from "@chakra-ui/react";
+import Head from "next/head";
+import Navbar from '../navbar';
+import VoxelDog from '../voxel-dog'
+import NoSsr from '../no-ssr'
+
+export default function Main({children, router}) {
+    return(
+    <Box as="main" pb={8}>
+        <Head>
+            <title>Clinton Wee</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
+        <Navbar path={router.asPath}/>
+        <Container maxW="container.md" pt={14}>
+            <NoSsr>
+            <VoxelDog/>
+            </NoSsr>
+            {children}
+        </Container>
+    </Box>
+    )
+}
---
+++ pages/works.js
@@ -0,0 +1,35 @@
+import {Container, Box, Heading, SimpleGrid, Divider} from '@chakra-ui/react'
+import Section from '../components/motion'
+import {WorkGridItem } from "../components/grid-item";
+import Layout from '../components/layouts/article'
+
+
+const Works = () => {
+    const timeCheck = '/timecheck.png'
+    const card2me = './card2me.png'
+    return(
+        <Layout>
+        <Container>
+            <Heading as='h3' fontSize={20} mb={4}>
+                Works
+            </Heading>
+            <SimpleGrid columns={[1,1,2]} gap={6}>
+                <Section>
+                    <WorkGridItem id='timecheck' title="TimeCheck" thumbnail={timeCheck}>
+                        A simple and clean timer, with multiple themes, cross-screen synchronization, built-in alert system, and password protection.
+                    </WorkGridItem>
+
+                </Section>
+                <Section>
+                    <WorkGridItem id='Card2Me' title="Card2Me" thumbnail={card2me}>
+                        Let a loved one know you&apos;re thinking of them by designing a custom E-card, using intuitive drag & drop interface, and sending it via link.
+                    </WorkGridItem>
+
+                </Section>
+            </SimpleGrid>
+        </Container>
+        </Layout>
+    )
+}
+
+export default Works
